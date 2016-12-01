f0145adc35257c75942cb55c25132bcbd5e18783
Ming <Ming>
2016-12-01 17:57:07 +0800

    origin

---
+++ weProject/pages/demo/demo.js
@@ -0,0 +1,27 @@
+// pages/demo/demo.js
+Page({
+  data:{
+    a:1,
+    b:2,
+    c:4,
+    firstDate:{
+      title: "ming",
+      cont:"this is a test"
+    }
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
