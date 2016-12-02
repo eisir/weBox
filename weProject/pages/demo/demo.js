c050a6d882fb574f1cb64a4e2852a93f775badb0
Ming <Ming>
2016-12-02 18:16:18 +0800

    new

--- weProject/pages/demo/demo.js
+++ weProject/pages/demo/demo.js
@@ -7,7 +7,87 @@ Page({
     firstDate:{
       title: "ming",
       cont:"this is a test"
+    },
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4]
+  },
+  showAs:function(){
+    wx.showActionSheet({
+      itemList: ['A', 'B', 'C'],
+      itemColor:"#f00",
+      success: function(res) {
+        if (!res.cancel) {
+          console.log(res.tapIndex)
+          console.log(res)
+        }
+      }
+    })
+  },
+  showMd:function(){
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      confirmText:'确定按钮',
+      showCancel: true,
+      success: function(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    })
+  },
+  showToast:function(){
+    wx.getNetworkType({
+      success: function(res) {
+        var networkType = res.networkType // 返回网络类型2g，3g，4g，wifi
+        console.log(networkType)
+      }
+    })
+    wx.getStorageInfo({
+      success: function(res) {
+        console.log(res.keys)
+        console.log(res.currentSize)
+        console.log(res.limitSize)
+      }
+    })
+    wx.showToast({
+      title: '成功',
+      icon: 'loading',
+      duration: 2000
+    })
+  },
+  switch: function(e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
     }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function(e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function(e){
+    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
