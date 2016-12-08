30d069029becfb69dff03b781acb874f9f702a77
Ming <Ming>
2016-12-08 18:13:36 +0800

    update

--- tinihuan/pages/verify/verify.js
+++ tinihuan/pages/verify/verify.js
@@ -1,6 +1,64 @@
 // pages/verify/verify.js
 Page({
-  data:{},
+  data:{
+    contList:[{
+      egSrc:'../../images/backcard.jpg',
+      egBig:'../../images/bigcard_2.jpg',
+      text:"身份证头像照",
+      img:""
+    },
+    {
+      egSrc:'../../images/facecard.jpg',
+      egBig:'../../images/bigcard_1.jpg',
+      text:"身份证国徽照",
+      img:""
+    },
+    {
+      egSrc:'../../images/catchcard.jpg',
+      egBig:'../../images/bigcard_3.jpg',
+      text:"手持身份证照",
+      img:""
+    }],
+    layerOptions:{
+      show:false,
+      imgSrc:"",
+      id:""
+    }
+  },
+  takePhoto:function(e){
+    var index=e.currentTarget.dataset.index;
+    var _this=this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        _this.data.contList[index].img=tempFilePaths[0]
+        console.log(_this.data.contList)
+        _this.setData({
+          contList : _this.data.contList
+        })
+      }
+    })
+  },
+  showEg:function(e){
+    this.data.layerOptions.show=true
+    this.data.layerOptions.id=(new Date()).getTime()
+    this.data.layerOptions.imgSrc=e.currentTarget.dataset.egbig
+    this.setData({
+      layerOptions : this.data.layerOptions
+    })
+  },
+  layerEvent:function(e){
+    if(e.target.dataset.id==this.data.layerOptions.id){
+      this.data.layerOptions.show=false;
+    }
+    this.setData({
+      layerOptions : this.data.layerOptions
+    })
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
